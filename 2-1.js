1fe8c0fa6ea136b5e048c86cde0446f757f54543
Cris Matson <crisjmatson@gmail.com>
2020-08-11 20:48:51 -0400

    first commit

---
+++ 2-1.js
@@ -0,0 +1,25 @@
+const entryForm = document.querySelector("form");
+
+const placeEntry = document.querySelector(".placeQ");
+const quantityEntry = document.querySelector(".quantityQ");
+const productEntry = document.querySelector(".productQ");
+const reviewSpace = document.querySelector(".bottom-wrapper");
+const reviewsList = document.querySelector(".ulReviews");
+
+entryForm.addEventListener("submit", logResults);
+
+function logResults(e) {
+	e.preventDefault();
+	let placeA = placeEntry.value;
+	let quantityA = quantityEntry.value;
+	let productA = productEntry.value;
+	let reviewEntry = document.createElement("li");
+
+	console.log(
+		`I've eaten at ${placeA} about ${quantityA} times and the ${productA} I was served last night was nothing short of horrendous.`
+	);
+	let newReview = `"I've eaten at ${placeA} about ${quantityA} times and the ${productA} I was served last night was nothing short of horrendous."`;
+
+	reviewsList.appendChild(reviewEntry);
+	reviewEntry.textContent = newReview;
+}
